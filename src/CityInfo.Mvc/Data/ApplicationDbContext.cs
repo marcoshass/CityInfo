514b6ec5669bd25080c99886d619e0165088ec55
hassmarcos <hass.marcos@bluetread.com>
2022-11-01 16:11:18 -0300

    About to change to ApplicationUser

---
+++ src/CityInfo.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CityInfo.Mvc.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.Mvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("identity");
+        }
+    }
+}
---
+++ src/CityInfo.Mvc/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CityInfo.Mvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
